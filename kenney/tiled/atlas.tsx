e794aebb5740b55f17ba1036f9b5f822bead0a01
sivanmg <sivanmg@hotmail.com>
2024-07-08 00:08:49 -0600

    fisrts commit
    fisrts commit

---
+++ kenney/tiled/atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="atlas" tilewidth="32" tileheight="32" tilecount="1222" columns="94">
+ <image source="../calles/set1/atlas.png" width="3036" height="416"/>
+</tileset>
